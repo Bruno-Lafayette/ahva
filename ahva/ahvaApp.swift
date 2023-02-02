82c627dcc44d7f35b3d19200b6b7e2ad207db107
Bruno.Lafayette <brunolafayette@rocketmail.com>
2023-02-02 10:29:57 -0300

    Initial Commit

---
+++ ahva/ahvaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ahvaApp.swift
+//  ahva
+//
+//  Created by Bruno Lafayette on 02/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ahvaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
